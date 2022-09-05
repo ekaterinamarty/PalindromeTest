9c132b507c100601499c85b9c985144986ad490c
Ekaterina <sonyamasonya@gmail.com>
2022-09-05 19:45:29 +0300

    *

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,23 +8,4 @@ public class App
 {
     public static void main(String[] args) { }
 
-    public static boolean isPalindrome(String phrase)
-    {
-        if (phrase == null || phrase.isBlank()) {
-            return false;
-        }
-        if (phrase.length() == 1 && !Character.isAlphabetic(phrase.charAt(0))) {
-            return false;
-        }
-        var i = 0;
-        var j = phrase.length() - 1;
-        while (i < j)
-        {
-            while (!Character.isAlphabetic(phrase.charAt(i))) i++;
-            while (!Character.isAlphabetic(phrase.charAt(j))) j--;
-            if (Character.toLowerCase(phrase.charAt(i++)) != Character.toLowerCase(phrase.charAt(j--)))
-                return false;
-        }
-        return true;
-    }
 }
